4ffe4a471b10e94078810627d2caf174f8ec065e
nickbar01234 <nickbar01234@gmail.com>
2023-05-15 23:37:57 -0400

    Add validation for school query
    Co-authored-by: cledi01 <caleb.gorrila@gmail.com>

--- routes/courses.route.ts
+++ routes/courses.route.ts
@@ -1,4 +1,5 @@
 import CONSTANTS from "@constants/";
+import puppeteer from "puppeteer";
 import { Code } from "@utils/code";
 import { Request, Router } from "express";
 import { query, validationResult } from "express-validator";
@@ -25,16 +26,39 @@ router.get(
     )
     .isString()
     .custom((value: string, meta) => {
-      return /\d{4}/.test(value);
+      return Object.values(CONSTANTS.terms).includes(value);
     })
     .withMessage("Terms must be a 4 digit number"),
-  (req: Request<any, any, any, Query>, res, next) => {
+  query("school")
+    .default("ALL")
+    .isString()
+    .custom((value: string, meta) => {
+      return Object.values(CONSTANTS.schools).includes(value);
+    }),
+  async (req: Request<any, any, any, Query>, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       console.log(errors.mapped);
       res.status(Code.badRequest).json(errors.mapped());
     }
 
+    const { term, school } = req.query;
+    // https://sis.it.tufts.edu/psp/paprd/EMPLOYEE/EMPL/h/?tab=TFP_CLASS_SEARCH#search_results/term/2238/career/ALL/subject/course/attr/keyword/instructor
+    // const url = `${CONSTANTS.baseUrl}/term/${term}/career/${school}/subject/course/attr/keyword/instructor`;
+    // console.log(url);
+    // const browser = await puppeteer.launch({ headless: false });
+    // const page = await browser.newPage();
+    // await page.goto(url);
+    // // tfp_accordion_row cls-show-js
+    // // await page.waitForSelector("#TFP_ClsSrch_viewport");
+    // await setTimeout(async () => {
+    //   console.log("hi");
+    //   // await page.waitForSelector("#TFP_search_results");
+    //   const html = await page.content();
+    //   console.log(html);
+    //   // await setTimeout(async () => await page.close(), 30000);
+    //   res.status(Code.ok).json(html);
+    // }, 3000);
     res.end();
   }
 );
